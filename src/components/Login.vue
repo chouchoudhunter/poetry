6f11319fdc5155ca50f16371b60315899dfade36
LAUXIAOYAN <wmzgrnwwxt@163.com>
2020-11-11 09:18:30 +0800

    修改登录注册

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,100 +1,114 @@
 <template>
   <div id="login">
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-    >
-      <el-menu-item index="1">登录</el-menu-item>
-      <el-menu-item index="2">注册</el-menu-item>
-    </el-menu>
-    <el-form
+    <div class="login-regist">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1">登录</el-menu-item>
+        <el-menu-item index="2">注册</el-menu-item>
+      </el-menu>
+    </div>
+    <div
       v-if="isShow1"
-      :model="ruleForm"
-      status-icon
-      :rules="rules"
-      ref="ruleForm"
-      label-width="100px"
-      class="demo-ruleForm"
+      class="login-content"
     >
-      <el-form-item
-        label="手机号"
-        prop="phone"
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="55px"
+        class="demo-ruleForm"
       >
-        <el-input
-          class="el-input"
-          type="text"
-          v-model="ruleForm.phone"
-          autocomplete="off"
-        ></el-input>
-      </el-form-item>
-      <el-form-item
-        label="密码"
-        prop="pass"
-      >
-        <el-input
-          type="password"
-          v-model="ruleForm.pass"
-          autocomplete="off"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
+        <el-form-item
+          label="用户名"
+          prop="name"
+        >
+          <el-input
+            size="small"
+            type="text"
+            v-model="ruleForm.name"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="密码"
+          prop="pass"
+        >
+          <el-input
+            size="small"
+            type="password"
+            show-password
+            v-model="ruleForm.pass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div class="ebutton">
         <el-button
           type="primary"
           @click="submitForm('ruleForm')"
-        >提交</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
-    <el-form
+        >登录</el-button>
+      </div>
+    </div>
+    <div
       v-if="isShow2"
-      ref="ruleForm"
-      :model="ruleForm"
-      status-icon
-      :rules="rules"
-      label-width="100px"
-      class="demo-ruleForm"
+      class="login-content"
     >
-      <el-form-item
-        label="手机号"
-        prop="phone"
+      <el-form
+        ref="ruleForm"
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        label-width="55px"
+        class="demo-ruleForm"
       >
-        <el-input
-          type="text"
-          v-model="ruleForm.phone"
-          autocomplete="off"
-        ></el-input>
-      </el-form-item>
-      <el-form-item
-        label="密码"
-        prop="pass"
-      >
-        <el-input
-          type="password"
-          v-model="ruleForm.pass"
-          autocomplete="off"
-        ></el-input>
-      </el-form-item>
-      <el-form-item
-        label="确认密码"
-        prop="checkPass"
-      >
-        <el-input
-          type="password"
-          v-model="ruleForm.checkPass"
-          autocomplete="off"
-        ></el-input>
-      </el-form-item>
-
-      <el-form-item>
+        <el-form-item
+          label="用户名"
+          prop="name"
+        >
+          <el-input
+            size="small"
+            type="text"
+            maxlength="16"
+            v-model="ruleForm.name"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="邮箱"
+          prop="email"
+        >
+          <el-input
+            size="small"
+            type="text"
+            v-model="ruleForm.email"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="密码"
+          prop="pass"
+        >
+          <el-input
+            size="small"
+            type="password"
+            show-password
+            v-model="ruleForm.pass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div class="ebutton">
         <el-button
           type="primary"
           @click="submitForm('ruleForm')"
         >注册</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -102,12 +116,23 @@
 export default {
   name: 'Login',
   data() {
-    var validatePhone = (rule, value, callback) => {
+    var validateName = (rule, value, callback) => {
       if (value === '') {
-        callback(new Error('请输入手机号'))
+        callback(new Error('请输入用户名'))
       } else {
-        if (!/^1[3456789]\d{9}$/.test(value)) {
-          callback(new Error('请输入正确的手机号'))
+        if (!/^[a-zA-Z0-9_\u4e00-\u9fa5]+$/.test(value)) {
+          callback(new Error('用户名由数字、字母、下划线组成，最长16位'))
+        } else {
+          callback()
+        }
+      }
+    }
+    var validateEmail = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入邮箱'))
+      } else {
+        if (!/^[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*\.[a-z]{2,}$/.test(value)) {
+          callback(new Error('邮箱格式错误'))
         } else {
           callback()
         }
@@ -117,49 +142,38 @@ export default {
       if (value === '') {
         callback(new Error('请输入密码'))
       } else {
-        if (this.ruleForm.checkPass !== '') {
-          this.$refs.ruleForm.validateField('checkPass')
-        }
         if (!/^(\w){8,16}$/.test(value)) {
           callback(new Error('密码长度在8~16之间，只能包含字母、数字和下划线'))
         }
         callback()
       }
     }
-    var validatePass2 = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请再次输入密码'))
-      } else if (value !== this.ruleForm.pass) {
-        callback(new Error('两次输入密码不一致!'))
-      } else {
-        callback()
-      }
-    }
     return {
       activeIndex: '1',
+      labelPosition: 'left',
       isShow1: true,
       isShow2: false,
       ruleForm: {
-        phone: '',
+        name: '',
+        email: '',
         pass: '',
-        checkPass: '',
       },
       rules: {
-        phone: [
+        name: [
           {
-            validator: validatePhone,
+            validator: validateName,
+            trigger: 'blur',
+          },
+        ],
+        email: [
+          {
+            validator: validateEmail,
             trigger: 'blur',
           },
         ],
         pass: [
           {
             validator: validatePass,
-            trigger: 'blur',
-          },
-        ],
-        checkPass: [
-          {
-            validator: validatePass2,
             trigger: 'blur',
           },
         ],
@@ -190,17 +204,43 @@ export default {
         }
       })
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
-    },
   },
 }
 </script>
 
 <style lang="scss">
-.demo-ruleForm{
-  .el-input{
-    width: 80%;
+#login {
+  padding: 0 5% 5%;
+  .login-regist {
+    width: 40%;
+    margin: 0 auto;
+    .el-menu-demo {
+      width: 100%;
+      margin: 0 auto;
+      border: none;
+      .el-menu-item {
+        width: 50%;
+        margin: 0 auto;
+        padding: 0;
+        text-align: center;
+      }
+    }
+  }
+  .login-content {
+    .demo-ruleForm {
+      margin: 20px auto;
+      .el-form-item {
+        width: 95%;
+        margin: 10px auto;
+      }
+    }
+    .ebutton {
+      width: 30%;
+      margin: 0 auto;
+      .el-button {
+        width: 100%;
+      }
+    }
   }
 }
 </style>
