2f2a2edcb1bbed8fa47aa7a916af5c7adb7c9a80
wuzhiyu <1002207631@qq.com>
2020-11-19 10:40:08 +0800

    fix login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,157 +1,84 @@
 <template>
   <div id="login">
-    <el-tabs v-if="isShow">
+    <el-tabs v-if="isShow" :stretch="true">
       <el-tab-pane label="登录">
         <div>
           <el-form
-            ref="ruleForm"
-            :model="ruleForm"
-            status-icon
+            ref="loginForm"
+            :model="loginForm"
+            :status-icon="true"
             :rules="rules"
+            label-position="left"
             label-width="55px"
             class="demo-ruleForm"
           >
-            <el-form-item
-              label="用户名"
-              prop="user"
-            >
-              <el-input
-                v-model="ruleForm.name1"
-                size="small"
-                type="text"
-                autocomplete="off"
-              ></el-input>
+            <el-form-item label="用户名">
+              <el-input v-model="loginForm.name" size="small" type="text" autocomplete="off"></el-input>
             </el-form-item>
-            <el-form-item
-              label="密码"
-              prop="pass1"
-            >
-              <el-input
-                v-model="ruleForm.pass1"
-                size="small"
-                type="password"
-                autocomplete="off"
-              ></el-input>
+            <el-form-item label="密码">
+              <el-input v-model="loginForm.password" size="small" type="password" autocomplete="off"></el-input>
             </el-form-item>
           </el-form>
         </div>
         <div class="bottom">
-          <el-button
-            type="primary"
-            @click="submitForm('ruleForm')"
-          >登录</el-button>
-          <el-link
-            target="_blank"
-            @click="showFind()"
-          >忘记密码</el-link>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-link target="_blank" @click="showFind()">忘记密码</el-link>
         </div>
       </el-tab-pane>
       <el-tab-pane label="注册">
         <el-form
-          ref="ruleForm"
-          :model="ruleForm"
-          status-icon
+          ref="regForm"
+          :model="regForm"
+          :status-icon="true"
           :rules="rules"
-          label-width="55px"
           class="demo-ruleForm"
+          label-position="left"
+          label-width="55px"
           :hide-required-asterisk="true"
         >
-          <el-form-item
-            label="邮箱"
-            prop="email"
-          >
-            <el-input
-              v-model="ruleForm.email"
-              size="small"
-              type="text"
-              autocomplete="off"
-            ></el-input>
+          <el-form-item label="邮箱" prop="username">
+            <el-input v-model="regForm.username" size="small" type="text" autocomplete="off"></el-input>
           </el-form-item>
-          <el-form-item
-            label="密码"
-            prop="pass"
-          >
-            <el-input
-              v-model="ruleForm.pass"
-              size="small"
-              type="password"
-              autocomplete="off"
-            ></el-input>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="regForm.password" size="small" type="password" autocomplete="off"></el-input>
           </el-form-item>
         </el-form>
         <div class="bottom">
-          <el-button
-            type="primary"
-            @click="submitForm('ruleForm')"
-          >注册</el-button>
+          <el-button type="primary" @click="submitForm('regForm')">注册</el-button>
         </div>
       </el-tab-pane>
     </el-tabs>
     <div v-if="!isShow">
       <div class="head">
-        <i
-          class="el-icon-arrow-left"
-          @click="!showFind()"
-        ></i>
+        <i class="el-icon-arrow-left" @click="!showFind()"></i>
         <div class="head-center">找回密码</div>
       </div>
       <el-form
-        ref="ruleForm"
-        :model="ruleForm"
-        status-icon
+        ref="forgotForm"
+        :model="forgotForm"
+        :status-icon="true"
         :rules="rules"
+        label-position="left"
         label-width="55px"
         class="demo-ruleForm"
         :hide-required-asterisk="true"
       >
-        <el-form-item
-          label="邮箱"
-          prop="email"
-        >
-          <el-input
-            v-model="ruleForm.email"
-            size="small"
-            type="text"
-            autocomplete="off"
-          ></el-input>
+        <el-form-item label="邮箱" prop="username">
+          <el-input v-model="forgotForm.username" size="small" type="text" autocomplete="off" ></el-input>
         </el-form-item>
-        <el-form-item
-          label="验证码"
-          prop="code"
-        >
-          <el-input
-            v-model="ruleForm.code"
-            oninput="value=value.replace(/[^\d]/g,'')"
-            maxlength="6"
-          ></el-input>
+        <el-form-item label="验证码" prop="code">
+          <el-input v-model="forgotForm.code" oninput="value=value.replace(/[^\d]/g,'')" maxlength="6"></el-input>
           <el-button>
-            <span
-              v-show="show"
-              @click="getCode"
-            >发送验证码</span>
-            <span
-              v-show="!show"
-              class="count"
-            >{{ count }} s</span>
+            <span v-show="show" @click="getCode">发送验证码</span>
+            <span v-show="!show" class="count">{{ count }} s</span>
           </el-button>
         </el-form-item>
-        <el-form-item
-          label="新密码"
-          prop="pass"
-        >
-          <el-input
-            v-model="ruleForm.pass"
-            size="small"
-            type="password"
-            autocomplete="off"
-          ></el-input>
+        <el-form-item label="新密码" prop="password">
+          <el-input v-model="forgotForm.password" size="small" type="password" autocomplete="off"></el-input>
         </el-form-item>
       </el-form>
       <div class="ebutton">
-        <el-button
-          type="primary"
-          @click="submitForm('ruleForm')"
-        >提交</el-button>
+        <el-button type="primary" @click="submitForm('forgotForm')">提交</el-button>
       </div>
     </div>
   </div>
@@ -166,44 +93,50 @@ export default {
       show: true,
       count: '',
       timer: null,
-      ruleForm: {
-        user: '',
-        pass1: '',
-        email: '',
-        pass: '',
+      loginForm: {
+        username: '',
+        password: '',
+      },
+      regForm: {
+        username: '',
+        password: '',
+      },
+      forgotForm: {
+        username: '',
         code: '',
+        password: '',
       },
       rules: {
-        email: [
+        username: [
           {
             required: true,
             message: '请输入邮箱地址',
-            trigger: 'blur',
+            trigger: ['blur', 'change'],
           },
           {
             type: 'email',
             message: '请输入正确的邮箱地址',
-            trigger: 'blur',
+            trigger: ['blur', 'change'],
           },
         ],
-        pass: [
+        password: [
           {
             required: true,
             message: '请输入密码',
-            trigger: 'blur',
+            trigger: ['blur', 'change'],
           },
           {
             type: 'string',
             pattern: /^(\w){8,16}$/,
             message: '密码长度在8~16之间，只能包含字母、数字和下划线',
-            trigger: 'blur',
+            trigger: ['blur', 'change'],
           },
         ],
         code: [
           {
             required: true,
             message: '请输入验证码',
-            trigger: 'blur',
+            trigger: ['blur', 'change'],
           },
         ],
       },
@@ -216,9 +149,12 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert('submit!')
+          return true
         } else {
-          alert('error submit!!')
+          this.$message({
+            message: '请检查输入是否正确！',
+            type: 'error',
+          })
           return false
         }
       })
@@ -245,26 +181,9 @@ export default {
 
 <style lang="scss">
 #login {
-  padding: 5%;
+  padding-top: 5px;
 
   .el-tabs {
-
-    .el-tabs__nav-wrap::after {
-      height: 0;
-    }
-
-    .el-tabs__nav-scroll {
-      width: 100%;
-      background: bisque;
-
-      .el-tabs__item {
-        width: 100px;
-        background: aquamarine;
-        padding: 0;
-        text-align: center;
-      }
-    }
-
     .el-tab-pane {
       width: 90%;
       margin: 0 auto;
@@ -273,10 +192,6 @@ export default {
         width: 100%;
         margin: 0 auto;
         text-align: center;
-
-        .el-button {
-          margin-right: 20px;
-        }
       }
     }
   }
