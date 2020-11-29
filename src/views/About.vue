f9e35ac600294328e455dd7d185fe414ce7cd414
LAUXIAOYAN <wmzgrnwwxt@163.com>
2020-11-29 14:14:41 +0800

    fix about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,33 +1,35 @@
 <template>
-  <div>
-    <marquee behavior="slide" direction="up">
-      <div class="about">
-        <font size="3em">
-          <pre>
-        三五个字凝成故事，
-        四五个韵独奏出岁月的回音。
-诗、词、曲即使横越过发落寞与复兴不断更替的时光，
-        仍然具有其自己独特的哲学意味与民族价值，
-        那些少男少艾欲言又止的情愫，
-        那些名人志士们浩荡激昂的心神，
-        用尽一生也无法踏遍的山川奇秀，
-  一世都难以参破禅缘佛与都凝在那短短三言两语里。
-  本网站企图开启一场属于中华诗词的文艺再复兴，
-        新的媒体形式将作为全新的载体，
-         每日一诗词作为传达方式，
-      所有人都是我们这场文化复兴之旅的目标对象，
-        让我们在文字里踏遍山川遍阅世事，
-      记住早已远去缥缈在眉间心上的哀愁与思念，
-      让我们记住贯穿在整个华夏大地的文化脉搏，
-        或许一切都从未远去......
-          </pre>
-        </font>
+  <div class="about">
+    <div class="about-content">
+      <marquee behavior="slide" direction="up">
+        <div class="text-style">
+          三五个字凝成故事，<br>
+          四五个韵独奏出岁月的回音。<br>
+          诗、词、曲即使横越过落寞与复兴不断更替的时光，<br>
+          仍然具有其自己独特的哲学意味与民族价值，<br>
+          那些少男少艾欲言又止的情愫，<br>
+          那些名人志士们浩荡激昂的心神，<br>
+          用尽一生也无法踏遍的山川奇秀，<br>
+          一世都难以参破禅缘佛与都凝在那短短三言两语里。
+          <br><br>
+          本网站企图开启一场属于中华诗词的文艺再复兴，<br>
+          新的媒体形式将作为全新的载体，<br>
+          每日一诗词作为传达方式，<br>
+          所有人都是我们这场文化复兴之旅的目标对象，<br>
+          让我们在文字里踏遍山川遍阅世事，<br>
+          记住早已远去缥缈在眉间心上的哀愁与思念，<br>
+          让我们记住贯穿在整个华夏大地的文化脉搏，<br>
+          或许一切都从未远去......<br>
+        </div>
+      </marquee>
+    </div>
+    <div class="about-bottom">
+      <div class="line"></div>
+      <div class="team">
+        <span @click="openLoginWindow1()">关于我们</span>
+        |
+        <span @click="openLoginWindow2()">联系我们</span>
       </div>
-    </marquee>
-    <div id="dingwei" >
-      <hr>
-      <div id="a" @click="openLoginWindow1()"><b>团队介绍</b></div>
-      <div id="b" @click="openLoginWindow2()"><b>联系我们</b></div>
     </div>
     <PopBox :visible.sync="popBoxShow1">
       <pre>
@@ -73,30 +75,38 @@ export default {
 </script>
 <style lang="scss">
 .about {
-  line-height: 200%;
-  text-align: center;
-  height: auto;
-  width: auto;
-  overflow: auto;
-  z-index: 0;
-}
+  height: 100%;
 
-#dingwei {
-  position: fixed;
-  width: 100%;
-  bottom: 15%;
-  left: 0;
-  height: 0;
-  z-index: 1;
-}
+  .about-content {
+    height: 95%;
+    overflow: auto;
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none; /* IE 10+ */
 
-#a {
-  float: left;
-  padding-left: 10%;
-}
+    .text-style {
+      line-height: 200%;
+      text-align: center;
+    }
+  }
 
-#b {
-  float: right;
-  padding-right: 10%;
+  .about-content::-webkit-scrollbar {
+    display: none; /* Chrome Safari */
+    -ms-overflow-style: none; /* IE 10+ */
+  }
+
+  .about-bottom {
+    height: 5%;
+
+    .line {
+      height: 2px;
+      margin: 5px 0;
+      background-color: rgb(255, 255, 255, 0.8);
+    }
+
+    .team {
+      font-weight: bold;
+      letter-spacing: 1px;
+    }
+  }
 }
 </style>
